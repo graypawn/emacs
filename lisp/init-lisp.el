bdcd7fdf85dd243b14df79d88b2b8100e2dd06aa
graypawn <choi.pawn@gmail.com>
2015-11-11 17:08:01 +0900

    Setup lispy-modes

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,6 +1,20 @@
-;rainbow-delimiters
 (require-package 'rainbow-delimiters)
-(add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
+
+(defun lisp-setup ()
+  (rainbow-delimiters-mode t))
+
+(defconst elispy-modes
+  '(emacs-lisp-mode ielm-mode)
+  "Major modes relating to elisp.")
+
+(defconst lispy-modes
+  (append elispy-modes
+	  '(lisp-mode inferior-lisp-mode lisp-interaction-mode))
+  "All lisp major modes.")
+
+(require 'derived)
+
+(dolist (hook (mapcar #'derived-mode-hook-name lispy-modes))
+  (add-hook hook 'lisp-setup))
 
 (provide 'init-lisp)
