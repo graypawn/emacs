ccceadb440c06a08e69e56f39156c862d8cb5d06
graypawn <choi.pawn@gmail.com>
2015-11-13 15:46:54 +0900

    python key-set like lisp

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,9 +6,13 @@
 (add-hook 'python-mode-hook (lambda ()
 			      (jedi:setup)
 			      (jedi:ac-setup)
-			      (local-set-key (kbd "C-c d") 'jedi:show-doc)
-			      (local-set-key (kbd "M-.") 'jedi:goto-definition)))
-
+			      (local-set-key (kbd "C-c M-j") 'run-python)
+			      (local-set-key (kbd "C-c C-c") 'python-shell-send-defun)
+			      (local-set-key (kbd "C-c C-k") 'python-shell-send-buffer)
+			      (local-set-key (kbd "C-c C-d d") 'jedi:show-doc)
+			      (local-set-key (kbd "M-.") 'jedi:goto-definition)
+			      (local-set-key (kbd "M-,") 'jedi:goto-definition-pop-marker)
+			      (local-set-key (kbd "C-c M-:") 'python-shell-send-string)))
 (setq jedi:complete-on-dot t)
 
 (add-hook 'python-mode-hook 'autopair-mode)
