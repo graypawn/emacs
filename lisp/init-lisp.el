05980dc3ce4d3b0e0d264d9ed5faa7c2343c86be
graypawn <choi.pawn@gmail.com>
2015-11-13 14:51:17 +0900

    Emacs Lisp setting

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,9 +1,43 @@
+(require 'ielm)
+
+(defvar elisp/repl-original-buffer nil
+  "Buffer from which we jumped to this REPL.")
+(make-variable-buffer-local 'elisp/repl-original-buffer)
+
+(defvar elisp/repl-switch-function 'switch-to-buffer-other-window)
+
+(defun elisp/switch-to-ielm ()
+  (interactive)
+  (let ((orig-buffer (current-buffer)))
+    (if (get-buffer "*ielm*")
+	(funcall elisp/repl-switch-function "*ielm*")
+      (ielm))
+    (setq elisp/repl-original-buffer orig-buffer)))
+
+(defun elisp/repl-switch-back ()
+  "Switch back to the buffer from witch we reached this REPL."
+  (interactive)
+  (if elisp/repl-original-buffer
+      (funcall elisp/repl-switch-function elisp/repl-original-buffer)
+    (error "No original buffer.")))
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-z") 'elisp/switch-to-ielm)
+(define-key ielm-map (kbd "C-c C-z") 'elisp/repl-switch-back)
+
+(define-key emacs-lisp-mode-map (kbd "C-c C-c") 'eval-defun)
+(define-key emacs-lisp-mode-map (kbd "C-c C-k") 'eval-buffer)
+
 (require-package 'rainbow-delimiters)
 
 (defun lisp-setup ()
   (rainbow-delimiters-mode t)
   (enable-paredit-mode)
   (turn-on-eldoc-mode))
+
+(require-package 'elisp-slime-nav)
+
+(defun emacs-lisp-setup ()
+  (turn-on-elisp-slime-nav-mode))
 
 (defconst elispy-modes
   '(emacs-lisp-mode ielm-mode)
@@ -18,5 +52,8 @@
 
 (dolist (hook (mapcar #'derived-mode-hook-name lispy-modes))
   (add-hook hook 'lisp-setup))
+
+(dolist (hook (mapcar #'derived-mode-hook-name elispy-modes))
+  (add-hook hook 'emacs-lisp-setup))
 
 (provide 'init-lisp)
