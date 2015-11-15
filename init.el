01218e6cfdcc411e7eb39822d95f98a09049087f
graypawn <choi.pawn@gmail.com>
2015-11-15 22:21:37 +0900

    modified:   init.el 	new file:   lisp/init-git.el

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-(require 'init-elpa)
+(require 'init-elpa) ; package utils
 (require 'init-defaults)
-(require 'init-themes)
+(require 'init-themes) ;color theme
 
+(require 'init-git)
 (require 'init-ido)
 (require 'init-auto-complete)
 (require 'init-edit)
@@ -13,32 +14,3 @@
 (require 'init-python)
 
 (provide 'init)
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
-;;  '(custom-safe-themes
-;;    (quote
-;;     ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))))
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("ac5584b12254623419499c3a7a5388031a29be85a15fdef9b94df2292d3e2cbb" "40664277ccd962bc373bff67affb4efa7c9bf3dabd81787e6e08fe080ba9645f" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
