9f52e16020ea339b3f9b1680fb84c06075e5d7db
graypawn <choi.pawn@gmail.com>
2015-11-15 22:47:53 +0900

    modified init-python.el

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,32 +1,31 @@
-(setq  python-shell-interpreter "ipython")
+;;Use IPython
+(setq python-shell-interpreter "ipython")
+(setq python-shell-buffer-name "IPython")
+(setq python-indent-guess-indent-offset nil)
 
 ;;jedi
+;;require 'M-x jedi:install-server'
 (require-package 'jedi)
-;; require 'M-x jedi:install-server'
-(add-hook 'python-mode-hook (lambda ()
-			      (jedi:setup)
-			      (jedi:ac-setup)
-			      (local-set-key (kbd "C-c M-j") 'run-python)
-			      (local-set-key (kbd "C-c C-c") 'python-shell-send-defun)
-			      (local-set-key (kbd "C-c C-k") 'python-shell-send-buffer)
-			      (local-set-key (kbd "C-c C-d d") 'jedi:show-doc)
-			      (local-set-key (kbd "M-.") 'jedi:goto-definition)
-			      (local-set-key (kbd "M-,") 'jedi:goto-definition-pop-marker)
-			      (local-set-key (kbd "C-c M-:") 'python-shell-send-string)))
 (setq jedi:complete-on-dot t)
 
+;;jedi keybind
+(setq jedi:key-show-doc (kbd "C-c C-d d"))
+(setq jedi:key-goto-definition (kbd "M-."))
+(setq jedi:key-goto-definition-pop-marker (kbd "M-,"))
+
+(defun python-setup ()
+  (jedi:setup)
+  (local-set-key (kbd "C-c M-j") 'run-python)
+  (local-set-key (kbd "C-c C-c") 'python-shell-send-defun)
+  (local-set-key (kbd "C-c C-k") 'python-shell-send-buffer)
+  (local-set-key (kbd "C-c M-:") 'python-shell-send-string))
+
+(add-hook 'python-mode-hook 'python-setup)
+;;autopair
 (add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'inferior-python-mode-hook 'autopair-mode)
+;;highlight indentation
 (require-package 'highlight-indentation)
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
 
 (provide 'init-python)
-
-
-
-
-
-
-
-
-
-
