720e4e1e44e15a1004aec43ce86fdf4d9abbd8f8
graypawn <choi.pawn@gmail.com>
2015-12-27 22:41:25 +0900

    Fix cc-mode

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -4,8 +4,8 @@
   (setq c-basic-offset 2))
 
 (with-eval-after-load 'cc-mode
-  (local-set-key (kbd "C-c k") 'recompile)
-  (local-set-key (kbd "C-c C-k") 'compile))
+  (define-key c-mode-base-map (kbd "C-c k") 'recompile)
+  (define-key c-mode-base-map (kbd "C-c C-k") 'compile))
 
 
 (add-to-list 'c-mode-common-hook 'c-mode-setup)
