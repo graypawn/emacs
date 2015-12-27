c9bec8c994b97b9647b6e2f6417ea35622226cbc
graypawn <choi.pawn@gmail.com>
2015-12-27 12:38:36 +0900

    use 'with-eval-after-load'
    	modified:   init-gtags.el
    	modified:   init-lisp.el
    	modified:   init-python.el
    
    	modified:   init-cc.el

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -1,8 +1,9 @@
 (defun c-mode-setup ()
   (ggtags-mode)
   (c-set-style "bsd")
-  (setq c-basic-offset 2)
-  
+  (setq c-basic-offset 2))
+
+(with-eval-after-load 'cc-mode
   (local-set-key (kbd "C-c k") 'recompile)
   (local-set-key (kbd "C-c C-k") 'compile))
 
