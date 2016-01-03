29f33cdc09b54255d4504b732e237e018ea2b425
graypawn <choi.pawn@gmail.com>
2016-01-03 23:20:28 +0900

    emacsclient font

--- lisp/init-os.el
+++ lisp/init-os.el
@@ -1,7 +1,14 @@
 ;;Linux Config
-(when (eq system-type 'gnu/linux)
-  ;;Font
-  (set-default-font "Hack-12" t)
-  (set-fontset-font "fontset-default" 'hangul '("NanumGothic" . "iso10646-1")))
+
+(if (daemonp)
+    (progn
+      (add-to-list 'default-frame-alist '(font . "Hack-12"))
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (with-selected-frame frame
+                    (set-fontset-font "fontset-default" 'hangul '("NanumGothic" . "iso10646-1"))))))
+  (when (eq system-type 'gnu/linux)
+    (set-default-font "Hack-12" t)
+    (set-fontset-font "fontset-default" 'hangul '("NanumGothic" . "iso10646-1"))))
 
 (provide 'init-os)
