2bd654414701a227b33ecc74c85976b6a37408f3
graypawn <choi.pawn@gmail.com>
2016-01-08 02:01:33 +0900

    일본어 폰트 오류 수정

--- lisp/init-os.el
+++ lisp/init-os.el
@@ -1,26 +1,47 @@
-
-(cond
- ((daemonp)
-  (progn
-    (add-to-list 'default-frame-alist '(font . "Hack-12"))
-    (add-hook
-     'after-make-frame-functions
-     (lambda (frame)
-       (with-selected-frame frame
-         (set-fontset-font "fontset-default"
-                           'hangul
-                           '("NanumGothic" . "iso10646-1"))
-         (set-fontset-font (frame-parameter nil 'font)
-                           'japanese-jisx0208
-                           '("VL Gothic" . "unicode-bmp")))))))
- ((eq system-type 'gnu/linux)
-  (progn
-    (message "This is Linux")
-    (set-frame-font "Hack-12" t)
-    (set-fontset-font "fontset-default" 'hangul
+(if (daemonp)
+    (progn
+      (add-to-list 'default-frame-alist '(font . "Hack-12"))
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (with-selected-frame frame
+                    (set-fontset-font "fontset-default"
+                                      'hangul
+                                      '("NanumGothic" . "iso10646-1"))
+                    (set-fontset-font (frame-parameter nil 'font)
+                                      'japanese-jisx0208
+                                      '("VL Gothic" . "unicode-bmp"))))))
+  (when (eq system-type 'gnu/linux)
+    (set-default-font "Hack-12" t)
+    (set-fontset-font "fontset-default"
+                      'hangul
                       '("NanumGothic" . "iso10646-1"))
     (set-fontset-font (frame-parameter nil 'font)
                       'japanese-jisx0208
-                      '("VL Gothic" . "unicode-bmp")))))
+                      '("VL Gothic" . "unicode-bmp"))))
+
+;; (cond
+;;  ((daemonp) (progn
+;;              (add-to-list 'default-frame-alist '(font . "Hack-12"))
+;;              (add-hook 'after-make-frame-functions
+;;                        (lambda (frame)
+;;                          (with-selected-frame frame
+;;                            (set-fontset-font "fontset-default"
+;;                                              'hangul
+;;                                              '("NanumGothic" . "iso10646-1"))
+;;                            (set-fontset-font (frame-parameter nil 'font)
+;;                                              'japanese-jisx0208
+;;                                              '("VL Gothic" . "unicode-bmp")))))))
+;;  ((eq system-type 'gnu/linux) (progn (set-default-font "Hack-12" t)
+;;                                      (set-fontset-font "fontset-default"
+;;                                                        'hangul
+;;                                                        '("NanumGothic" . "iso10646-1"))
+;;                                      (set-fontset-font (frame-parameter nil 'font)
+;;                                                        'japanese-jisx0208
+;;                                                        '("VL Gothic" . "unicode-bmp")))))
+
+
+
 
 (provide 'init-os)
+ 
+                           
