3b89ecaff89d19202d050d81e050b45f4985029d
graypawn <choi.pawn@gmail.com>
2016-01-10 00:31:46 +0900

    Pandoc와 함께 markdown 사용

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,15 +1,28 @@
 (require-package 'markdown-mode)
 
-;;http://increasinglyfunctional.com/2014/12/18/github-flavored-markdown-previews-emacs/
-;;위 링크에서 '수정한' ruby script를 공개했다.
+(defun convert-markdown-to (newtype)
+  "Converts the current buffer, assumed to be in Markdown format,
+into a new format.  The new format must be one acceptable to
+`Pandoc'.  The function opens the new file in a buffer if called
+interactively.  If not called interactively then it returns the
+name of the new file."
+  (interactive "sOutput: ")
+  (let ((current-document (buffer-file-name))
+        (temp-filename (concat "/tmp/output." newtype)))
+    (with-temp-file temp-filename
+      (call-process-shell-command (concat "pandoc -f markdown -t " newtype)
+                                  nil t nil current-document))
+    (cond ((called-interactively-p 'any)
+           (with-current-buffer (find-file temp-filename)))
+          (t temp-filename))))
 
+(with-eval-after-load 'markdown-mode
+  (define-key markdown-mode-map (kbd "C-c c") 'convert-markdown-to))
 
-(setq markdown-open-command "/usr/local/bin/mark")
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
-
-(setq markdown-command "/usr/local/bin/flavor")
+(setq markdown-command "pandoc -s --highlight-style zenburn -f markdown -t html")
 
 (provide 'init-markdown)
