5c07d37da2712fdd651688eb717a6c7ae52bd987
graypawn <choi.pawn@gmail.com>
2016-02-18 22:10:20 +0900

    ido merge 끄기

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,8 +1,8 @@
 (ido-mode t)
 (ido-everywhere t)
 
-(setq ido-use-filename-at-point nil)
+(setq ido-use-filename-at-point t)
 (setq ido-enable-flex-matching t)
+(setq ido-auto-merge-work-directories-length -1)
 
 (provide 'init-ido)
-
