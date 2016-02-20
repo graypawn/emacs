41e6e5a87e7d8aec248eb4006cb89a31b9f6ebcc
graypawn <choi.pawn@gmail.com>
2016-02-21 02:25:58 +0900

    SBCL의 실행에 --noinform 옵션 추가

--- lisp/init-clisp.el
+++ lisp/init-clisp.el
@@ -1,4 +1,4 @@
-(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq inferior-lisp-program "/usr/bin/sbcl --noinform")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
 (require 'slime)
 (slime-setup '(slime-fancy))
