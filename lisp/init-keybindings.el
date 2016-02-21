9bd4edcf0078f719a1ee1c399ec88387facb9497
graypawn <choi.pawn@gmail.com>
2016-02-21 21:54:54 +0900

    C-l을 prefix로 사용해 keybind 재배치

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -1,29 +1,31 @@
-;;Useful Shortcut
+;;; Useful Shortcut
 (global-unset-key (kbd "C-z"))
 (global-unset-key (kbd "C-x C-z"))
+(global-unset-key (kbd "C-l"))          ;C-l은 prefix로 사용한다.
 
+;;; Eshell
 (global-set-key (kbd "C-x m") 'eshell)
 (global-set-key (kbd "C-x M") (lambda () (interactive) (eshell t))) ;one more eshell
 
 (global-set-key (kbd "C-x C-m") 'execute-extended-command) ;M-x
 
-;;Buffer Shortcut
+;;; Buffer Shortcut
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;; Other Window backward
+;;; Other Window backward
 (global-set-key (kbd "C-x O") (lambda ()
                                 (interactive)
                                 (other-window -1)))
 
 ;;; Org
-(global-set-key (kbd "C-c o")
+(global-set-key (kbd "C-l n")
                 (lambda () (interactive)
                   (find-file org-default-notes-file)))
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c l") 'deft)
+(global-set-key (kbd "C-l c") 'org-capture)
+(global-set-key (kbd "C-l a") 'org-agenda)
+(global-set-key (kbd "C-l l") 'org-store-link)
+(global-set-key (kbd "C-l d") 'deft)
 
-;;Apropos
+;;; Apropos
 (global-set-key (kbd "C-h l") 'apropos-library)
-
 (provide 'init-keybindings)
