e0a20d570453973498e9fc130b4ad29db4bcc138
graypawn <choi.pawn@gmail.com>
2016-03-01 01:04:49 +0900

    필요 없는 것들 삭제

--- lisp/init-graphviz.el
+++
@@ -1,8 +0,0 @@
-(require-package 'graphviz-dot-mode)
-
-(with-eval-after-load 'graphviz-dot-mode
-  (define-key graphviz-dot-mode-map (kbd "C-c k") 'recompile)
-  (define-key graphviz-dot-mode-map (kbd "C-c C-k") 'compile))
-
-
-(provide 'init-graphviz)
