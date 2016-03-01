fb111cb273c8f62aba29f06435c9d827914dfa79
graypawn <choi.pawn@gmail.com>
2016-03-02 00:54:06 +0900

    주석 위치 변경

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -1,9 +1,11 @@
-;;; eshell에서 맨 앞에 붙는 "$" 문구를 변경한다.
 (with-eval-after-load 'eshell
+  ;;; eshell에서 맨 앞에 붙는 "$" 문구를 변경한다.
   (setq eshell-prompt-function
         (lambda ()
           (concat
            (car (last (split-string (eshell/pwd) "/")))
            " $ "))))
+
+
 
 (provide 'init-eshell)
