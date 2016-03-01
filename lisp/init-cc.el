5cba88157d0a980c100ecd901adfe94034a4cae4
graypawn <choi.pawn@gmail.com>
2016-03-02 00:53:19 +0900

    cpp 레퍼런스 확인을 위한 cclookup 추가.

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -7,6 +7,21 @@
   (hs-minor-mode t)
   (autopair-mode t))
 
+;;; cclookup
+;;; cpp 레퍼런스를 offline에서 확인할 수 있다.
+;;; https://github.com/tsgates/cclookup/tree/master
+(setq cclookup-dir (expand-file-name "cclookup" user-emacs-directory))
+(add-to-list 'load-path cclookup-dir)
+
+(eval-when-compile (require 'cclookup))
+(setq cclookup-program (concat cclookup-dir "/cclookup.py"))
+(setq cclookup-db-file (concat cclookup-dir "/cclookup.db"))
+
+(autoload 'cclookup-lookup "cclookup"
+  "Lookup SEARCH-TERM in the Python HTML indexes." t)
+(autoload 'cclookup-update "cclookup" 
+  "Run cclookup-update and create the database at `cclookup-db-file'." t)
+
 ;;; CC-mode Key Binding
 (with-eval-after-load 'cc-mode
   (define-key c-mode-base-map (kbd "C-c k") 'recompile)
@@ -15,8 +30,12 @@
   (define-key c-mode-base-map (kbd "C-c S") 'hs-show-all)
   (define-key c-mode-base-map (kbd "C-c H") 'hs-hide-all)
 
+  ;; c++-mode
+  (define-key c++-mode-map (kbd "C-c C-d h") 'cclookup-lookup)
+
   ;; java-mode
   (define-key java-mode-map (kbd "C-c C-d h") 'javadoc-lookup))
+
 
 (add-to-list 'c-mode-common-hook 'cc-mode-setup)
 (provide 'init-cc)
