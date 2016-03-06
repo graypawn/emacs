bd43632e16c235f62f9457799367694e4b433519
graypawn <choi.pawn@gmail.com>
2016-03-06 21:51:36 +0900

    Warnning 해결 : "reference to free variable"

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -3,6 +3,7 @@
 
 (add-hook 'clojure-mode-hook 'lisp-setup)
 
+(eval-when-compile (require 'clojure-mode))
 (with-eval-after-load 'clojure-mode
   (define-key clojure-mode-map (kbd "C-c C-z") 'cider-jack-in))
 
