92dfb9b84114d4fd4cd760870faa6ddf89eaffc2
graypawn <choi.pawn@gmail.com>
2016-03-06 11:31:07 +0900

    git에서 제외되는 Custom.el 생성.

--- init.el
+++ init.el
@@ -28,4 +28,11 @@
 (require 'init-python)
 (require 'init-scheme)
 ;; (require 'init-perl)
+
+;;; Load "custom.el"
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 (provide 'init)
+
