823e8779a2d2080e2536c8642efe447c3e3892b5
graypawn <choi.pawn@gmail.com>
2016-03-16 08:22:15 +0900

    Sample을 최소화 했다.

--- sample/graypawn-modules.el
+++ sample/graypawn-modules.el
@@ -1,18 +1,16 @@
 (require 'init-org)
-(require 'init-helm)
+(require 'init-helm)                    ;C-f, C-b가 helm 함수로 되어있다.
 (require 'init-eshell)
 
 (require 'init-auto-complete)
-(require 'init-gtags)
-
-(require 'init-tw)
+;; (require 'init-gtags)
+;; (require 'init-tw)
 
 (require 'init-lisp)
-
-(require 'init-clisp)
-(require 'init-clojure)
-(require 'init-cc)
-(require 'init-java)
-(require 'init-python)
-(require 'init-scheme)
+;; (require 'init-cc)
+;; (require 'init-clisp)
+;; (require 'init-clojure)
+;; (require 'init-java)
+;; (require 'init-python)
+;; (require 'init-scheme)
 ;; (require 'init-perl)
