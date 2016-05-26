74f22b3f807f510a4856298c19173df8bf6a96ae
graypawn <choi.pawn@gmail.com>
2016-05-26 21:28:01 +0900

    Fix java snippet.

--- auto-insert/template.java
+++ auto-insert/template.java
@@ -4,6 +4,6 @@
  */
 package ${1:`(mapconcat 'identity (split-string (replace-regexp-in-string ".*src\\(/\\(main\\|test\\)\\)?\\(/java\\)?" "" default-directory) "/" t) ".")`};
 
-public class ${1:`(file-name-sans-extension (buffer-name))`}$2 {
+public class ${2:`(file-name-sans-extension (buffer-name))`}$3 {
     $0
 }
